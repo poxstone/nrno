658152cf8579f9e46ff17d7cb77217b47bb5cfde
poxstone <poxstone@gmail.com>
2015-09-30 03:42:32 +0000

    popup and animate

--- public/js/nrn-dsn.js
+++ public/js/nrn-dsn.js
@@ -1,47 +1,3 @@
-(function(funcName, baseObj) {
-    funcName = funcName || "docReady";
-    baseObj = baseObj || window;
-    var readyList = [];
-    var readyFired = false;
-    var readyEventHandlersInstalled = false;
-    function ready() {
-        if (!readyFired) {
-            readyFired = true;
-            for (var i = 0; i < readyList.length; i++) {
-                if(readyList[i].fn){
-                  readyList[i].fn.call(window, readyList[i].ctx);
-                }
-            }
-            readyList = [];
-        }
-    }
-    function readyStateChange() {
-        if ( document.readyState === "complete" ) {
-            ready();
-        }
-    }
-    baseObj[funcName] = function(callback, context) {
-        if (readyFired) {
-            setTimeout(function() {callback(context);}, 1);
-            return;
-        } else {
-            readyList.push({fn: callback, ctx: context});
-        }
-        if (document.readyState === "complete") {
-            setTimeout(ready, 1);
-        } else if (!readyEventHandlersInstalled) {
-            if (document.addEventListener) {
-                document.addEventListener("DOMContentLoaded", ready, false);
-                window.addEventListener("load", ready, false);
-            } else {
-                document.attachEvent("onreadystatechange", readyStateChange);
-                window.attachEvent("onload", ready);
-            }
-            readyEventHandlersInstalled = true;
-        }
-    }
-})("docReady", window);
-
 function preventDefault(evento){
   if(event.preventDefault){ evento.preventDefault() }else{ evento.returnValue = false; }
 }
@@ -82,7 +38,11 @@ var show_popUp = {
     preventDefault(evento);
     //copia contenido
     //oculta popup
-    show_popUp.rmClass(vars.body,'show-popUp');
+    show_popUp.addClass(vars.body,'hide-popUp');
+    setTimeout(function(){
+      show_popUp.rmClass(vars.body,'show-popUp');
+      show_popUp.rmClass(vars.body,'hide-popUp');
+    },690);
   },
   ini: function(){
     show_popUp.vars = {};
