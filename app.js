20a8c4b99044851faf2a89d2d9d27807a8dcda63
poxstone <poxstone@gmail.com>
2015-11-17 04:23:32 +0000

    prety code

--- app.js
+++ app.js
@@ -51,6 +51,7 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
+    app.locals.pretty = true;
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
